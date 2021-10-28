20e82676c8501fcc64a2f27c60d05015f157a547
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-10-28 22:48:18 +0530

    header work

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./header/Header";
+import Home from "./pages/home/Home";
 import TopBar from "./topbar/TopBar";
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Header />
+      <Home />
     </>
   );
 };
