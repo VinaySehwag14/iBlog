b48ffb1270681d9658291eed543a4276171e25a4
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-11-03 02:33:32 +0530

    new component done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./component/header/Header";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
-import TopBar from "./topbar/TopBar";
+import TopBar from "./component/topbar/TopBar";
+import Write from "./pages/write/Write";
 
 const App = () => {
   return (
     <>
       <TopBar />
-      <Single />
+      <Write />
       {/* <Home /> */}
     </>
   );
