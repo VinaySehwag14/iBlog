1022e02674a50adfad55ee4afa337591bea7b67f
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-11-07 02:07:45 +0530

    ERROR IN LOGIN AND REGISTER

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,23 @@ import TopBar from "./component/topbar/TopBar";
 import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const user = true;
   return (
-    <>
+    <BrowserRouter>
       <TopBar />
-      <Login />
-      {/* <Home /> */}
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="register" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="write" element={user ? <Write /> : <Register />} />
+        <Route path="setting" element={user ? <Setting /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
