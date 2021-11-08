952cbf7cbac04e94ba770967793f62ca53a19052
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-11-09 02:01:20 +0530

    NETLIFY ERROR FIXED

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./component/header/Header";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import TopBar from "./component/topbar/TopBar";
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,4 +1,3 @@
-import Post from "../../component/post/Post";
 import Sidebar from "../../component/sidebar/Sidebar";
 import SinglePost from "../../component/singlePost/SinglePost";
 import "./single.css";
